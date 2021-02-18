8e7f4f2e89b2782f1c44e752559d8c800821c7a8
unknown <selvaraj.php@gmail.com>
2021-02-17 22:27:32 -0700

    build config changes

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 
 // In production we need to pass these values in instead of relying on webpack
 setup(app, {
-  outputPath: resolve(process.cwd(), 'build'),
+  outputPath: resolve(process.cwd(), ''),
   publicPath: '/',
 });
 
